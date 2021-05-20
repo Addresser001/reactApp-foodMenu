b4326a830533444cb3454c73097f3b4691385284
addresser <sokarithankgod2@gmail.com>
2021-05-19 20:43:17 -1100

    Added Mobile view styles

--- src/navBar.js
+++ src/navBar.js
@@ -1,38 +1,62 @@
+import { useState } from "react"
+
 const NavBar = ({data, setItems}) => {
+    const [isActive, setActive] = useState(false)
 
     const filterAll=()=>{
     
         setItems(data)
+        setActive(!isActive);
     
     }
 
     const filterBreakfast=()=>{
         let newItems= data.filter((item)=> item.category === 'breakfast');
         setItems(newItems)
+        setActive(!isActive);
 
     }
 
     const filterLunch=()=>{
         let newItems= data.filter((item)=> item.category === 'lunch');
         setItems(newItems) 
+        setActive(!isActive);
     }
     const filterShakes=()=>{
         let newItems= data.filter((item)=> item.category === 'shakes');
-        setItems(newItems) 
+        setItems(newItems)
+        setActive(!isActive); 
     }
+
+
+    const handleToggle = () => {
+        setActive(!isActive);
+    };
     
     return ( 
         <div className="Navigation-container">
-            <div className="title">
-                <h1>Our Menu</h1>
-                <i class="fa fa-minus"></i>
+            <div className="Mobile">
+                <div className="MobileNav">
+                    <i class="fa fa-align-left" onClick={handleToggle}></i>
+                    <div className="mobileTitle">
+                        <h1 >Our Menu</h1>
+                        <i class="fa fa-minus"></i>
+                    </div>
+                </div>
             </div>
+            <div  className={isActive ? "NavBar-mobileShow" : "NavBar-mobileHide"} >
+                <span class="material-icons" onClick={handleToggle}>close</span>
+                <div className="title">
+                    <h1>Our Menu</h1>
+                    <i class="fa fa-minus"></i>
+                </div>
 
-            <div className="NavButtons">
-                <button onClick={()=>filterAll()}>all</button>
-                <button onClick={()=>filterBreakfast()} >breakfast</button>
-                <button onClick={()=>filterLunch()}>lunch</button>
-                <button onClick={()=>filterShakes()}>shakes</button>
+                <div className="NavButtons">
+                    <button onClick={()=>filterAll()}>all</button>
+                    <button onClick={()=>filterBreakfast()} >breakfast</button>
+                    <button onClick={()=>filterLunch()}>lunch</button>
+                    <button onClick={()=>filterShakes()}>shakes</button>
+                </div>
             </div>
         </div>
     );
